b0519926a51d454ab976b575d0bab4e3856027a3
Jeremy Kwa <jkwa@ucsd.edu>
2017-11-15 14:29:46 -0800

    Set up project and added board class test file

---
+++ tic-tac-toe/src/board.clj
@@ -0,0 +1,7 @@
+(ns board
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "I'm a litto teapot!"))
---
+++ tic-tac-toe/test/board_test.clj
@@ -0,0 +1,7 @@
+(ns board-test
+  (:require [clojure.test :refer :all]
+            [board :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
