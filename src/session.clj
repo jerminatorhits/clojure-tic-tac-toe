b7e49fbcb8e3a827ef56763b010a6bd0db593a78
Jeremy Kwa <jkwa@ucsd.edu>
2017-11-27 10:38:07 -0800

    Update formatting of welcome message

--- src/session.clj
+++ src/session.clj
@@ -3,4 +3,4 @@
 
 (defn session-start
   []
-  "Welcome to Tic-Tac-Toe! You will be 'X' and the computer will be 'O'")
+  "Welcome to Tic-Tac-Toe! You will be 'X' and the computer will be 'O'\nThe spaces available are labeled 1 through 9. Enter the space you wish to mark.")
--- test/session_test.clj
+++ test/session_test.clj
@@ -4,4 +4,4 @@
 
 (deftest session-test
   (testing "prints welcome message upon start"
-    (is (session-start) "Welcome to Tic-Tac-Toe! You will be 'X' and the computer will be 'O'")))
+    (is (session-start) "Welcome to Tic-Tac-Toe! You will be 'X' and the computer will be 'O'\nThe spaces available are labeled 1 through 9. Enter the space you wish to mark.")))
