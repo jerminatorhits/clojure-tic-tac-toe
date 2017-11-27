3aafa920b1e63f4c68b4a910f5a60be0517e262b
Jeremy Kwa <jkwa@ucsd.edu>
2017-11-27 09:14:45 -0800

    Add a board with create-board

--- src/board.clj
+++ src/board.clj
@@ -6,7 +6,12 @@
   [& args]
   (println "I'm a litto teapot!"))
 
+(def board [1 2 3 4 5 6 7 8 9])
+
 (defn create-board
   []
   [1 2 3 4 5 6 7 8 9])
 
+(defn update-board
+  [board location mark]
+  (assoc board location mark))
--- test/board_test.clj
+++ test/board_test.clj
@@ -1,7 +1,10 @@
 (ns board-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test :refer [deftest testing is]]
             [board :refer :all]))
 
-(deftest board
+(deftest board-test
   (testing "creates an empty board"
-    (is (= (create-board) [1 2 3 4 5 6 7 8 9]))))
+    (is (= [1 2 3 4 5 6 7 8 9] (create-board))))
+
+  (testing "updates board when given a location and mark"
+    (is (= [:X 2 3 4 5 6 7 8 9] (update-board [1 2 3 4 5 6 7 8 9] 0 :X)))))
