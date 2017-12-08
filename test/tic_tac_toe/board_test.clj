bdc8830c549530c004a817fd6c91394b1c21f56e
Jeremy Kwa <jkwa@ucsd.edu>
2017-12-08 13:14:28 -0800

    refactor input validation function

--- test/tic_tac_toe/board_test.clj
+++ test/tic_tac_toe/board_test.clj
@@ -7,7 +7,7 @@
     (is (= [1 2 3 4 5 6 7 8 9] (create-board))))
 
   (testing "updates board when given a location and mark"
-    (is (= ["X" 2 3 4 5 6 7 8 9] (update-board [1 2 3 4 5 6 7 8 9] 0 "X")))))
+    (is (= ["X" 2 3 4 5 6 7 8 9] (update-board [1 2 3 4 5 6 7 8 9] 1 "X")))))
 
 ;;  (testing "formats board into presentable string when given a board"
 ;;    (is (= (format-board [1 2 3 4 5 6 7 8 9]) "\n 1 | 2 | 3 \n---|---|---\n 4 | 5 | 6 \n---|---|---\n 7 | 8 | 9 \n")))
--- test/tic_tac_toe/session_test.clj
+++ test/tic_tac_toe/session_test.clj
@@ -4,23 +4,32 @@
             [tic-tac-toe.session :refer :all]))
 
 (deftest session-test
-  (testing "prints welcome message upon start"
-    (is (session-welcome) "Welcome to Tic-Tac-Toe! You will be 'X' and the computer will be 'O'\nThe spaces available are labeled 1 through 9. Enter the space you wish to mark."))
+  (testing "validates input from player correctly"
+    (is (= (validate-integer 1) true)))
   
   (testing "validates input from player correctly"
-    (is (= (validate-entry 1) true)))
+    (is (= (validate-integer 0) false)))
   
   (testing "validates input from player correctly"
-    (is (= (validate-entry 0) false)))
+    (is (= (validate-integer -1) false)))
   
   (testing "validates input from player correctly"
-    (is (= (validate-entry -1) false)))
+    (is (= (validate-integer "a") false)))
   
-  (testing "validates input from player correctly"
-    (is (= (validate-entry "a") false)))
+  (testing "checks to see if position is already taken"
+    (is (= (empty-space? ["X" 2 3 4 5 6 7 8 9] 1) false)))
   
   (testing "prompts player for a valid move at the beginning of the turn"
-    (is (with-in-str "1" (session-prompt "Enter the space you wish to mark: ")) 0))
+    (is (with-in-str "1" (session-turn [1 2 3 4 5 6 7 8 9] "X")) ["X" 2 3 4 5 6 7 8 9]))
 
   (testing "prompts player for a valid move if invalid move is provided"
-    (is (with-in-str "p\n1" (session-prompt "Enter the space you wish to mark: ")) 0)))
+    (is (with-in-str "p\n1" (session-turn [1 2 3 4 5 6 7 8 9] "X")) ["X" 2 3 4 5 6 7 8 9])))
+  
+  ;(testing "processes a turn correctly"
+  ;  (is (= (turn player [1 2 3 4 5 6 7 8 9]) ["X" 2 3 4 5 6 7 8 9]  ))))
+  
+  ;(testing "game-loop can run one cycle"
+  ;  (let [current-board (create-board) human (create-player "X") computer (create-player "O") turn human]
+  ;    ()
+  ;  (is (= (game-loop current-board human computer turn)))))
+  ;)
