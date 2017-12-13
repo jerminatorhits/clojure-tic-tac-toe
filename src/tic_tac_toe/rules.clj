e456bcc53820c9ffd9a9d365d2f2e36849ef0f3c
Jeremy Kwa <jkwa@ucsd.edu>
2017-12-12 22:13:21 -0800

    Change :refer :all and refactor rules using board AND token as arguments

--- src/tic_tac_toe/rules.clj
+++ src/tic_tac_toe/rules.clj
@@ -1,33 +1,40 @@
 (ns tic-tac-toe.rules
     (require [tic-tac-toe.board :refer :all]))
 
-(defn horizontal-win [board]
-  (or
-   (= (count (distinct (subvec board 0 3))) 1)
-   (= (count (distinct (subvec board 3 6))) 1)
-   (= (count (distinct (subvec board 6 9))) 1)))
+(defn horizontal-win [board token]
+  (if
+   (or 
+    (= token (apply str (distinct (subvec board 0 3))))
+    (= token (apply str (distinct (subvec board 3 6))))
+    (= token (apply str (distinct (subvec board 6 9)))))
+    token))
 
-(defn vertical-win [board]
-  (or 
-   (= (count (distinct (take-nth 3 board))) 1)
-   (= (count (distinct (take-nth 3 (drop 1 board)))) 1)
-   (= (count (distinct (take-nth 3 (drop 2 board)))) 1)))
+(defn vertical-win [board token]
+  (if
+   (or 
+    (= token (apply str (distinct (take-nth 3 board))))
+    (= token (apply str (distinct (take-nth 3 (drop 1 board)))))
+    (= token (apply str (distinct (take-nth 3 (drop 2 board))))))
+    token))
 
-(defn diagonal-win [board]
-  (= (nth board 0) (nth board 4) (nth board 8)))
+(defn diagonal-win [board token]
+  (if (= (nth board 0) (nth board 4) (nth board 8) token) token))
 
-(defn antidiagonal-win [board]
-  (= (nth board 2) (nth board 4) (nth board 6)))
+(defn antidiagonal-win [board token]
+  (if (= (nth board 2) (nth board 4) (nth board 6) token) token))
 
-(defn winner? [board]
-  (or
-   (horizontal-win board)
-   (vertical-win board)
-   (diagonal-win board)
-   (antidiagonal-win board)))
+(defn winner? [board token]
+  (if
+   (or
+    (= token (horizontal-win board token))
+    (= token (vertical-win board token))
+    (= token (diagonal-win board token))
+    (= token (antidiagonal-win board token)))
+    token
+    false))
 
-(defn tie? [board]
-  (and (false? (winner? board)) (true? (nil? (some number? board)))))
+(defn tie? [board token]
+  (and (false? (winner? board token)) (true? (nil? (some number? board)))))
 
 (defn game-over? [board]
   (or (winner? board) (tie? board)))
